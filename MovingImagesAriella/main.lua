4bc034cb28d527d3da3d6c37d94f41f9f53649b9
Ariella Harlequin <ariella.harlequin@ocsbstudent.ca>
2020-03-02 15:18:06 -0500

    updated

--- MovingImagesAriella/main.lua
+++ MovingImagesAriella/main.lua
@@ -38,3 +38,28 @@ local function MoveBeetleShip(event)
 end
 
 -- MoveShip will be called over and over again
+Runtime:addEventListener("enterFrame", MoveBeetleShip)
+
+-- character image with width and height
+local girl4 = display.newImageRect("Images/girl4.png", 200, 200)
+
+-- set the image to be transparent
+girl4.alpha = 0
+
+-- set the intial x and y of girl4
+girl4.x = 1000
+girl4.y = display.contentHeight/2
+
+--Function: MovieShip
+--Input: this function accepts an event listener
+--Output: none
+--Description: This function adds the scroll speed to the x-value of the ship
+local function Movegirl4(event)
+	-- add the scroll to the x-value of the ship
+	girl4.x = girl4.x - scrollSpeed
+	-- change the transparancy of the ship every time it moves so that it fades out 
+	girl4.alpha = girl4.alpha + 0.01
+end
+
+-- MoveShip will be called over and over again
+Runtime:addEventListener("enterFrame", Movegirl4)
