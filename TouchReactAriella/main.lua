9da2c031fef447bc96060589d24a9a5556262914
Ariella Harlequin <ariella.harlequin@ocsbstudent.ca>
2020-03-09 14:23:08 -0400

    added touch and react

--- TouchReactAriella/main.lua
+++ TouchReactAriella/main.lua
@@ -3,9 +3,29 @@
 -- Course: ICS2O
 -- This program shows two images that when clicked shows text and 
 -- the second image
-
+-----------------------------------------------------------------------------
 
 -- set background colour
-display.setDefault ("backround", /, /, /)
+display.setDefault ("backround", 63/255, 155/255, 129/255)
 
--- hide the status
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- create blue button, set its position and make it invisible
+local redButton = display.newImageRect("Images/red_x.png", 198,96)
+redButton.x = display.contentWidth/2
+redButton.y = displaycontentHeight/2
+redButton.isVisible = true
+
+-- create red button, set its position and make it invisible
+local greenButton = display.newImageRect("Images/checkmark.png", 198,96)
+redButton.x = display.contentWidth/2
+redButton.y = displaycontentHeight/2
+redButton.isVisible = false
+
+-- create the text object, set it's position and make it invisible 
+local textObject = display.newText ("Clicked!", 0, 0, nil, 50)
+textObject.x = display.contentWidth/2
+textObject.y = displaycontentHeight/2
+textObject: setTextColour (1, 1, 0)
+textObject.isVisible = false
