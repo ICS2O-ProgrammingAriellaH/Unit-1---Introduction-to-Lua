f7ff5b8b220b3528918634929eb955223c0cde47
Ariella Harlequin <ariella.harlequin@ocsbstudent.ca>
2020-03-24 13:24:37 -0400

    added area of a rectangle and hello world

--- HelloWorldAriella/main.lua
+++ HelloWorldAriella/main.lua
@@ -4,20 +4,26 @@
 -----------------------------------------------------------------------------------------
 
 -- print a greeting to the console
-print ("****Hello Ariella!")
+print ("****Hola Shalissa!")
 
 -- hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
 
 -- set the color of the background
-display.setDefault("background", 203/255, 61/255, 225/255)
+display.setDefault("background", 102/255, 173/255, 213/255)
 
 -- create a local vaiable
 local textObject
 
 -- displays text on the screen at position x = 500 and y = 500 with
 -- a default font style and font size of 50
-textObject = display.newText( "Hello, Ariella!", 500, 500, nil, 50 )
+textObject = display.newText( "Hola, Shalissa!", 550, 250, nil, 110 )
 
 -- sets the color to the color of the text
-textObject:setTextColor(189/255, 0/255, 32/255)
+textObject:setTextColor(56/255, 220/255, 45/255)
+
+-- this displays another text that says created by Ariella
+textObject = display.newText( "Created By: Ariella:)", 550, 550, nil, 85 )
+
+-- sets the color to the color of the text
+textObject:setTextColor(227/255, 164/255, 17/255)
